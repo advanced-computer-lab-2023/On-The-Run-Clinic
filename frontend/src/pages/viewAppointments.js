63028039de8c9240f0cf0348339ed1c2a5668ca6
AhmedYasser17x <ahmedyasser.x@hotmail.com>
2023-11-13 04:12:06 +0200

    appointment reservation

--- frontend/src/pages/viewAppointments.js
+++ frontend/src/pages/viewAppointments.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams , useNavigate} from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
+import PaymentForm from '../components/Stripe';
+import { loadStripe } from '@stripe/stripe-js';
+const stripePromise = loadStripe('your-publishable-key');
 
 const ViewAppointments = () => {
   const { doctorUsername, patientUsername } = useParams();
+  const [patient,setPatient]=useState('')
   const navigate = useNavigate();
   const [doctorId, setDoctorId] = useState(null);
   const [appointments, setAppointments] = useState([]);
   const [loading, setLoading] = useState(true);
   const [patientId , setPatientId] = useState(null);
+  const [familyMembers, setFamilyMembers] = useState([]);
+  const [selectedFamilyMember , setSelectedFamilyMember] = useState([null]); 
+  const [selectedLinkedPatient, setSelectedLinkedPatient] = useState(null);
+ const [formData, setFormData] = useState({
+    selectedAppointment: '', // Store the selected package ID
+    paymentMethod: 'creditCard',
+    username:patientUsername,
+  });
 
   useEffect(() => {
     const fetchDoctorId = async () => {
@@ -32,6 +45,7 @@ const ViewAppointments = () => {
             const response = await axios.get(`http://localhost:4000/getPatientByUsername/${patientUsername}`);
             if (response.status === 200) {
                 setPatientId(response.data._id);
+                setPatient(response.data);
                 console.log(patientId);
             }
         }
@@ -55,9 +69,23 @@ const ViewAppointments = () => {
       }
     };
 
+    const fetchFamilyMembers = async () => {
+        try {
+         
+          const response = await axios.get(`http://localhost:4000/getFamilyMem/${patientUsername}`);
+          console.log(response.data);
+  
+          if (response.status === 200) {
+            setFamilyMembers(response.data);
+          }
+        } catch (error) {
+          console.error('Error fetching family members:', error);
+        } };
+
     fetchDoctorId();
     fetchPatientId();
     fetchAppointments();
+    fetchFamilyMembers();
   }, [doctorUsername, doctorId]);
 
   const reserveAppointment = async (appointmentId) => {
@@ -67,6 +95,7 @@ const ViewAppointments = () => {
         patientId,
         status: 'Scheduled',
         description: 'Checkup',
+        paymentMethod : formData.paymentMethod
       });
       if (response.status === 200) {
         // Handle success, maybe show a success message or update the UI
@@ -78,6 +107,64 @@ const ViewAppointments = () => {
       console.error('Error reserving appointment:', error);
     }
   };
+
+  const reserveFamilyMemberAppointment = async (appointmentId) => {
+    try {
+        console.log("APP ID" + appointmentId);
+        console.log("Request Data:", {
+            patientId,
+            status: 'Scheduled',
+            description: 'Checkup',
+            familyMemberId :selectedFamilyMember,
+            paymentMethod: formData.paymentMethod,
+          });
+        const response = await axios.post(`http://localhost:4000/reserveFamilyMemberAppointment/${appointmentId}`, {
+            patientId,
+            status: 'Scheduled',
+            description: 'Checkup',
+            familyMemberId : selectedFamilyMember,
+            paymentMethod : formData.paymentMethod
+          });
+          if (response.status===200) {
+            alert("Appointment reserved successfully");
+            console.log("Appointment reserved successfully");
+            window.location.reload();
+          }
+    }
+    catch(error) {
+        console.error('Error reserving appointment:', error);
+    }
+  };
+
+  const reserveLinkedPatientAppointment = async (appointmentId) => {
+    try {
+      console.log("APP ID " + appointmentId);
+      console.log("Request Data:", {
+        patientId,
+        status: 'Scheduled',
+        description: 'Checkup',
+        linkedPatientId: selectedLinkedPatient,
+        paymentMethod: formData.paymentMethod,
+      });
+
+      const response = await axios.post(`http://localhost:4000/reserveLinkedPatientAppointment/${appointmentId}`, {
+        patientId,
+        status: 'Scheduled',
+        description: 'Checkup',
+        linkedPatientId: selectedLinkedPatient,
+        paymentMethod: formData.paymentMethod,
+      });
+
+      if (response.status === 200) {
+        alert("Appointment reserved successfully");
+        console.log("Appointment reserved successfully");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error('Error reserving appointment:', error);
+    }
+  };
+  
 
   return (
     <div>
@@ -91,17 +178,83 @@ const ViewAppointments = () => {
               <p>Date: {appointment.date}</p>
               <p>Hour: {appointment.hour}</p>
               <p>Status: {appointment.status}</p>
-              {/* Add more appointment details as needed */}
               <button onClick={() => reserveAppointment(appointment._id)}>Reserve</button>
+              <div>
+                <label>Select Family Member:</label>
+                <select onChange={(e) => setSelectedFamilyMember(e.target.value)}>
+                  <option value={null}>Select Family Member</option>
+                  {familyMembers.map((familyMember) => (
+                    <option key={familyMember._id} value={familyMember._id}>
+                      {familyMember.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <button onClick={() => reserveFamilyMemberAppointment(appointment._id)}>
+                Reserve for Family Member
+              </button>
+              <div>
+                <label>Select Linked Patient:</label>
+                <select onChange={(e) => setSelectedLinkedPatient(e.target.value)}>
+                  <option value={null}>Select Linked Patient</option>
+                  {patient.linkedPatients.map((linkedPatient) => (
+                    <option key={linkedPatient.linkedPatientId} value={linkedPatient.linkedPatientId}>
+                      {linkedPatient.linkedPatientName} - {linkedPatient.linkedPatientRelation}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <button onClick={() => reserveLinkedPatientAppointment(appointment._id)}>
+                Reserve for Linked Patient
+              </button>
+              <div>
+                <label>Payment Method:</label>
+                <label>
+                  <input
+                    type="radio"
+                    name="paymentMethod"
+                    value="creditCard"
+                    checked={formData.paymentMethod === 'creditCard'}
+                    onChange={(e) => setFormData({ ...formData, paymentMethod: e.target.value })}
+                  />
+                  Credit Card
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="paymentMethod"
+                    value="wallet"
+                    checked={formData.paymentMethod === 'wallet'}
+                    onChange={(e) => setFormData({ ...formData, paymentMethod: e.target.value })}
+                  />
+                  Wallet
+                </label>
+              </div>
             </li>
           ))}
         </ul>
       ) : (
         <p>No available appointments.</p>
       )}
+      <h2>Payment Information</h2>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ flex: 1 }}>
+          <Elements stripe={stripePromise}>
+            <div className="stripe-container">
+              <PaymentForm />
+            </div>
+          </Elements>
+        </div>
+        <div style={{ flex: 1 }}>
+          {/* Add the Wallet form or content here */}
+        </div>
+      </div>
       <button onClick={() => navigate(-1)}>Back</button>
     </div>
   );
-};
+  
+  
+  
+  };
 
 export default ViewAppointments;
