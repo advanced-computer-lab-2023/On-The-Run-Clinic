8a6a5331863ce878884364696ae88252f17cb364
Farida Mousa <didahishamdh@gmail.com>
2023-11-28 00:07:53 +0200

    view details of selected prescirption and download selectd prescription

--- frontend/src/pages/viewMyPrescriptions.js
+++ frontend/src/pages/viewMyPrescriptions.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { PDFDocument } from 'pdf-lib';
+import jsPDF from 'jsPDF'
 
 const MyPrescription = () => {
   const { username } = useParams();
@@ -59,6 +61,44 @@ const MyPrescription = () => {
     setSelectedPrescription(null);
   };
 
+  const handleDownloadPrescription = async(prescription) => {
+    //create pdf
+    const pdfGenerate=()=>{
+      try{
+      var doc = new jsPDF('landscape', 'px', 'a4',false);
+      doc.addPage();
+      doc.setFont('Helvetica', 'bold');
+      doc.text(60,60,'Medication Name: ');
+      doc.text(60,80,'Dosage: ');
+      doc.text(60,100,'Instructions: ');
+      doc.text(60,120,'Date: ');
+      doc.text(60,140,'Doctor ID: ');
+      doc.text(60,160,'Filled: ');
+      doc.setFont('Helvetica','normal');
+      doc.text(100,60, `${prescription.medicationName}`);
+      doc.text(100,80, `${prescription.dosage}`);
+      doc.text(100,100, `${prescription.instructions}`);
+      doc.text(100,120, `${prescription.date}`);
+      doc.text(100,140, `${prescription.doctor}`);
+      doc.text(100,160, `${prescription.filled ? 'Yes' : 'No'}`);
+      doc.save('prescription.pdf');
+      
+      //create download link and trigger download
+      const url = URL.createObjectURL(doc.output('blob'));
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 'prescription.pdf';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      }
+      catch (error){
+        console.error('error creating pdf: ', error);
+      }
+      };
+      pdfGenerate();
+  };
+
   return (
     <div>
       <h1>Prescriptions of {username}</h1>
@@ -106,6 +146,7 @@ const MyPrescription = () => {
              Medication Name: {prescription.medicationName}<br />
              
              <button onClick={() => handleViewPrescription(prescription)}>View Prescription</button>
+             <button onClick={() => handleDownloadPrescription(prescription)}>Download Prescription</button>
            </li>
           ))}
         </ul>
