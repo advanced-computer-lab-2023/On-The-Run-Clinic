56d8817f48b1e21be65f7407950fbed6e560362c
LailaTalalThabet <Laylathabet1@gmail.com>
2023-12-02 20:24:43 +0200

    delete dr and delete admin

--- frontend/src/pages/DeleteDoctorPage.js
+++ frontend/src/pages/DeleteDoctorPage.js
@@ -1,22 +1,30 @@
 
-
-
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import BeatLoader from "react-spinners/BeatLoader";
+import { faEye, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Modal from 'react-modal';
+
 
 import './List.css'; // Import your CSS file for styling
+import DoctorDetailsModal from '../components/doctorDetailsModal';
 
 const DeleteDoctor = () => {
   const [doctors, setDoctors] = useState([]);
   const [loading, setLoading] = useState(true);
-  
- 
+  const [modalOpen, setModalOpen] = useState(false);
+  const [doctor, setDoctor] = useState("");
+  const [activeDoctorId, setActiveDoctorId] = useState(null);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+
+
+
 
   const fetchDoctors = async () => {
     try {
-      const response = await axios.get(`http://localhost:4000/getDoctors`,{
+      const response = await axios.get(`http://localhost:4000/getDoctors`, {
         withCredentials: true
       });
 
@@ -33,47 +41,127 @@ const DeleteDoctor = () => {
   useEffect(() => {
     fetchDoctors();
   }, []);
+
+
+
   const handleDelete = async (doctorId) => {
+    setIsDeleting(true);
     try {
       // Make a DELETE request to the backend to delete the patient
-      await axios.delete(`http://localhost:4000/deleteDoctor/${doctorId}`,{
+      await axios.delete(`http://localhost:4000/deleteDoctor/${doctorId}`, {
         withCredentials: true
       });
 
       // After successful deletion, refresh the patient list by re-fetching
       fetchDoctors();
+      setIsDeleting(false);
     } catch (error) {
       console.error('Error deleting doctor:', error);
+    } finally {
+     
     }
   };
 
   return (
-    <div className="medicine-list-container">
-      <h1>All Doctors</h1>
-      
-      {loading ? (
-        <p>Loading...</p>
-      ) : doctors.length > 0 ? (
-        <ul className="medicine-list">
-          {doctors.map((m) => (
-            <li key={m._id} className="medicine-item">
-              <div className="medicine-details">
-                <strong>Name:</strong> {m.name}<br />
-                <strong>Username:</strong> {m.username}<br />
-                <strong>email:</strong> {m.email}<br />
-               
-                <Link to="#" onClick={() => handleDelete(m._id)}>Delete</Link>
-                
-                
-              </div>
-              
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No Doctors found.</p>
-      )}
-    </div>
+    <div className="container">
+      <div className="patients-list">
+        <h2>All Doctors</h2>
+
+
+        {loading || isDeleting ? (
+          <div className="spinner-container">
+            <BeatLoader color="#14967f" size={15} />
+          </div>
+        ) : doctors.length === 0 ? (
+          <p>No doctors found</p>
+        ) : (
+          <ul className="patients-list">
+            {doctors.map((m) => (
+
+              <li key={m._id}>
+                <div className="patients-header">
+                  <div style={{ flex: 1, textAlign: 'left' }}>
+                    <strong>USername: </strong>{m.username}
+                  </div>
+                  <div style={{ flex: 1, textAlign: 'left' }}>
+                    <strong>ID: </strong>{m._id}
+                  </div>
+                  <div style={{ flex: 1, textAlign: 'right', marginRight: '10px' }}>
+                    <FontAwesomeIcon
+                      className="view-icon"
+                      icon={faEye}
+                      style={{ marginLeft: '10px' }}
+                      onClick={() => {
+                        setModalOpen(true);
+                        setActiveDoctorId(m._id)
+                        setDoctor(doctors.find((p) => p._id === activeDoctorId));
+                      }}
+                    />
+                    <FontAwesomeIcon
+                      className="delete-icon"
+                      icon={faTrash}
+                      style={{ marginLeft: '10px' }}
+                      onClick={() => {
+                        setIsConfirmModalOpen(true)
+                        setActiveDoctorId(m._id)
+                        setDoctor(doctors.find((p) => p._id === activeDoctorId));}
+
+                      }
+                        
+                    />
+                  </div>
+                </div>
+              </li>
+
+            ))}
+          </ul>
+
+        )}
+      </div>
+      {modalOpen && doctor &&
+        <DoctorDetailsModal
+          setOpenModal={setModalOpen}
+          doctor={doctor}
+        />
+      }
+      <Modal
+        isOpen={isConfirmModalOpen}
+        onRequestClose={() => setIsConfirmModalOpen(false)}
+        contentLabel="Confirm Delete"
+        style={{
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: '#f4f4f4',
+            borderRadius: '10px',
+            padding: '20px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+        }}
+      >
+        <h2 style={{ color: '#333', marginBottom: '20px' }}>Confirm Delete</h2>
+        <p style={{ color: '#555', marginBottom: '30px' }}>Are you sure you want to delete this doctor?</p>
+       
+        <div>
+          <button style={{ marginRight: '10px', padding: '10px 20px', backgroundColor: 'crimson', color: '#fff', border: 'none', borderRadius: '5px' }} onClick={() => {
+            handleDelete(activeDoctorId);
+            setIsConfirmModalOpen(false);
+          }}>
+            Yes
+          </button>
+          <button style={{ padding: '10px 20px', backgroundColor: 'blue', color: '#fff', border: 'none', borderRadius: '5px' }} onClick={() => setIsConfirmModalOpen(false)}>
+            No
+          </button>
+        </div>
+      </Modal>
+    </div >
   );
 };
 
--- frontend/src/pages/ManageAdmins.js
+++ frontend/src/pages/ManageAdmins.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faPlus } from '@fortawesome/free-solid-svg-icons';
 import AdminForm from '../components/AdminForm';
+import Modal from 'react-modal';
 
 const ManageAdmins = () => {
 
   const [isFormVisible, setIsFormVisible] = useState(false);
   const [admins, setAdmins] = useState([]);
+  const [activeAdminId, setActiveAdminId] = useState(null);
+  const [admin, setAdmin] = useState("");
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
 
   const fetchAdmins = async () => {
     try {
@@ -54,6 +57,7 @@ const ManageAdmins = () => {
             className="add-icon"
             icon={faPlus}
             onClick={() => setIsFormVisible(true)}
+            style={{ color: '#14967f' }}
           />
         </h2>
         <ul>
@@ -63,10 +67,17 @@ const ManageAdmins = () => {
                 <div className="prescription-header">
                   <span><strong>Username: </strong>  {admin.username}</span>
                   <FontAwesomeIcon
-                    className="view-icon"
-                    icon={faTrash}
-                    onClick={() => handleDelete(admin._id)}
-                  />
+                      className="delete-icon"
+                      icon={faTrash}
+                      style={{ marginLeft: '10px' }}
+                      onClick={() => {
+                        setIsConfirmModalOpen(true)
+                        setActiveAdminId(admin._id)
+                       }
+
+                      }
+                        
+                    />
                 </div>
               </div>
             </li>
@@ -74,8 +85,45 @@ const ManageAdmins = () => {
         </ul>
       </div>
       <div className="prescription-form">
-        {isFormVisible && <AdminForm onAdminAdded={handleAdminAdded}/>}
+        {isFormVisible && <AdminForm onAdminAdded={handleAdminAdded} />}
       </div>
+      <Modal
+        isOpen={isConfirmModalOpen}
+        onRequestClose={() => setIsConfirmModalOpen(false)}
+        contentLabel="Confirm Delete"
+        style={{
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: '#f4f4f4',
+            borderRadius: '10px',
+            padding: '20px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+        }}
+      >
+        <h2 style={{ color: '#333', marginBottom: '20px' }}>Confirm Delete</h2>
+        <p style={{ color: '#555', marginBottom: '30px' }}>Are you sure you want to delete this doctor?</p>
+       
+        <div>
+          <button style={{ marginRight: '10px', padding: '10px 20px', backgroundColor: 'crimson', color: '#fff', border: 'none', borderRadius: '5px' }} onClick={() => {
+            handleDelete(activeAdminId);
+            setIsConfirmModalOpen(false);
+          }}>
+            Yes
+          </button>
+          <button style={{ padding: '10px 20px', backgroundColor: 'blue', color: '#fff', border: 'none', borderRadius: '5px' }} onClick={() => setIsConfirmModalOpen(false)}>
+            No
+          </button>
+        </div>
+      </Modal>
 
     </div>
 
