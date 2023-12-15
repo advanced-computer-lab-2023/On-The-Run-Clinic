cf63565fb231a3cd01c027dffc716959cfca8c4b
LailaTalalThabet <Laylathabet1@gmail.com>
2023-12-15 19:37:14 +0200

    admin

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -5,46 +5,58 @@ import { useAuthContext } from '../hooks/useAuthContext'
 import {useNavigate } from 'react-router-dom';
 
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserTie,faUserDoctor,faHospitalUser,faFileMedical,faKitMedical } from '@fortawesome/free-solid-svg-icons';
 const AdminDashboard = () => {
   const navigate = useNavigate();
   const { username } = useParams();
   
   return(
-  <div className="admin-dashboard">
-    <h1>Admin Dashboard</h1>
-    <ul className="admin-menu">
-      <li>
-        <Link to="/deleteAdmin" className="menu-link">
-          <i className="fas fa-users"></i>
-          Manage Admins
-        </Link>
-      </li>
-      <li>
-        <Link to="/deleteDoctor" className="menu-link">
-          <i className="fas fa-user-md"></i>
-          Manage Doctors
-        </Link>
-      </li>
-      <li>
-        <Link to="/deletePatient" className="menu-link">
-          <i className="fas fa-clipboard-list"></i>
-          Manage Patients
-        </Link>
-      </li>
-      <li>
-        <Link to="/viewRequests" className="menu-link">
-          <i className="fas fa-clipboard-list"></i>
-          View Requests
-        </Link>
-      </li>
-      <li>
-        <Link to="/healthPackages" className="menu-link">
-          <i className="fas fa-clipboard-list"></i>
-          Manage Health Packages
-        </Link>
-      </li>
-    </ul>
-  </div>
+ 
+
+
+
+
+
+
+
+
+
+<div className="admin-dashboard" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '90vh' }}>
+<div className="expandable-div" style={{ boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)', padding: '20px', width: '275px', height: '400px', margin: '20px' }}>
+ 
+  <Link to={`/deleteAdmin`} >
+  <FontAwesomeIcon style={{fontSize:'210px',color:'white',marginTop:'20px',marginBottom:'20px'}} icon={faUserTie} />
+    <p style={{ color: 'white', fontSize: '35px', marginBottom: '20px' }}>Manage Admins</p>
+  </Link>
+</div>
+<div className="expandable-div" style={{ boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)', padding: '20px', width: '275px', height: '400px', margin: '20px' }}>
+  <Link to={`/deleteDoctor`} >
+  <FontAwesomeIcon style={{fontSize:'210px',color:'white',marginTop:'20px',marginBottom:'20px'}} icon={faUserDoctor} />
+  <p style={{ color: 'white', fontSize: '35px', marginBottom: '18px' }}>Manage Doctors</p>
+  </Link>
+</div>
+<div className="expandable-div" style={{ boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)', padding: '20px', width: '275px', height: '400px', margin: '20px' }}>
+  <Link to={`/deletePatient`} >
+  <FontAwesomeIcon style={{fontSize:'210px',color:'white',marginTop:'20px',marginBottom:'20px'}} icon={faHospitalUser} />
+    <p style={{ color: 'white', fontSize: '35px', marginBottom: '18px' }}>Manage Patients</p>
+  </Link>
+</div>
+<div className="expandable-div" style={{ boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)', padding: '20px', width: '275px', height: '400px', margin: '20px' }}>
+  <Link to={`/viewRequests`} >
+  <FontAwesomeIcon style={{fontSize:'210px',color:'white',marginTop:'20px',marginBottom:'20px'}} icon={faFileMedical} />
+  <p style={{ color: 'white', fontSize: '35px', marginBottom: '18px' }}>Manage Requests</p>
+  </Link>
+</div>
+<div className="expandable-div" style={{ boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)', padding: '20px', width: '275px', height: '400px', margin: '20px' }}>
+  <Link to={`/healthPackages`} >
+  <FontAwesomeIcon style={{fontSize:'210px',color:'white',marginTop:'20px',marginBottom:'20px'}} icon={faKitMedical} /><p style={{ color: 'white', fontSize: '35px', marginBottom: '18px' }}>Manage Packages</p>
+  </Link>
+</div>
+</div>
+
+
+
 );
 };
 
